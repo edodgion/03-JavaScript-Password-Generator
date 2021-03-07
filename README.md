e01a3e4520afc2c31d31a2a634c1812f16c6d2ce
Eric Dodgion <Barry.manowar@gmail.com>
2021-03-07 15:55:15 -0700

    ready to turn in

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # 03-JavaScript-Password-Generator
+This week’s homework requires you to modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that you write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+
+The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN prompted for character types to include in the password
+THEN I choose lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+## Live Site Link
+
+https://edodgion.github.io/03-JavaScript-Password-Generator/
+
+## Screen Shots
+![](./assets/images/generate1.png)
+![](./assets/images/generate2.png)
+![](./assets/images/generatecode.png)
+![](./assets/images/generatecode2.png)
+
+## Credit
+This page was made with ❤️  by Eric Dodgion 03/05/2021
--- script.js
+++ script.js
@@ -1,24 +1,11 @@
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN prompted for character types to include in the password
-//THEN I choose lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
-//THEN a password is generated that matches the selected criteria
-//WHEN the password is generated
-//THEN the password is either
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var uppercase_characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
-var lowercase_characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
-var number_characters  = ["0","1","2","3","4","5","6","7","8","9"];
-var special_characters = ["!","#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","`","{","|","}","~"];
+var uppercaseArray = ["A", "B", "C","D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var numberArray  = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialArray = ["!","#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","`","{","|","}","~"];
+
 
 //Start generate password
 function  generatePassword() {
@@ -33,28 +20,28 @@ function  generatePassword() {
 
    //confirm for lowercase
    var userLowerCase = window.confirm("Click 'OK' if you would you like lowercase characters?");
-   if (userLowerCase == true || userLowerCase == null) {
+   if (userLowerCase == true) {
      options = options.concat(lowercaseArray)
    }
     //confirm for UPPERCASE
      var userUpperCase = window.confirm("Click 'OK' if you would you like UPPERCASE characters?");
-   if (userUpperCase == true || userUpperCase == null) {
+   if (userUpperCase == true) {
      options = options.concat(uppercaseArray)
     
    }
    
    //confirm for Numeric
    var userNumericCharacter = window.confirm("Click 'OK' if you would you like Numeric characters?");
-   if (userNumericCharacter == true || userNumericCharacter == null) {
+   if (userNumericCharacter == true) {
      options = options.concat(numberArray)
    }
 
     //confirm for Special Charaters
      var userSpecialCharacter = window.confirm("Click 'OK' if you would you like Special Characters characters?");
-   if (userSpecialCharacter == true || userSpecialCharacter == null) {
+   if (userSpecialCharacter == true) {
     options = options.concat(specialArray)
    }
-
+//Shouts out a "Try Again" window if user 'clicks' cancel for each character option
    if (options.length === 0) {
     alert("Try Again")
      return options;
@@ -80,12 +67,3 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
-
- 
- 
- 
- 
- 
-
-
